e4d20e7c55765ebfdee9431b66395f2a397c2026
Angelit Leonardo <leonardo.angelit@solera.com>
2022-10-19 10:29:10 +0200

    first test

--- src/test/java/com/note/persist/NotePersist/NotePersistApplicationTests.java
+++ src/test/java/com/note/persist/NotePersist/NotePersistApplicationTests.java
@@ -1,13 +1,29 @@
 package com.note.persist.NotePersist;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class NotePersistApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void createNote_WhenCorrectArguments_ReturnsTrue() {
+
+		final NoteController noteController = new NoteController();
+
+		final LocalDateTime local = LocalDateTime.now();
+
+		final LocalDateTime local2 = LocalDateTime.now();
+
+		final bool isCreated = noteController.createNote(id,
+				"description", "text", local, local2, "link",
+				"mentions", 'p');
+
+		Assert.isTrue(isCreated, "Note must have all the requisites");
+
 	}
 
 }

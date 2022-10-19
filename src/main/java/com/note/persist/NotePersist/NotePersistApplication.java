84eca314a388872d142a90dedccd79223f6eda07
MiguelCabero <Miguel.Cabero@solera.com>
2022-10-19 10:06:42 +0200

    initial commit

---
+++ src/main/java/com/note/persist/NotePersist/NotePersistApplication.java
@@ -0,0 +1,13 @@
+package com.note.persist.NotePersist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotePersistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotePersistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/note/persist/NotePersist/NotePersistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.persist.NotePersist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotePersistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
